bc7e12b18642e4203e1b6de1b567b1aa10a7c839
Craig Bossie <cbbossie@amazon.com>
2022-11-24 19:09:07 -0500

    Dax Cluster

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -64,3 +64,104 @@ resource "aws_memorydb_cluster" "reinvent_memdb" {
   snapshot_retention_limit = 7
   subnet_group_name        = aws_memorydb_subnet_group.reinventsubnetgroup.name
 }
+
+
+#DynamoDB Table - Oreder
+resource "aws_dynamodb_table" "orders" {
+  name         = "Orders"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "Id"
+
+  attribute {
+    name = "Id"
+    type = "N"
+  }
+
+  attribute {
+    name = "CustomerId"
+    type = "S"
+  }
+
+  attribute {
+    name = "Year"
+    type = "N"
+  }
+
+  global_secondary_index {
+    name               = "CustomerIdYear"
+    hash_key           = "CustomerId"
+    range_key          = "Year"
+    projection_type    = "INCLUDE"
+    non_key_attributes = ["Id"]
+  }
+}
+
+#Dax
+
+#Role for Dax
+resource "aws_iam_role" "dax_table_role" {
+  name = "dax_role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = "Assume"
+        Principal = {
+          Service = "dax.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
+#Policy for table
+resource "aws_iam_role_policy" "dax_table_policy" {
+  name = "dax-dynamodb-policy"
+  role = aws_iam_role.dax_table_role.name
+  policy = jsonencode(
+    {
+      Version : "2012-10-17",
+      Statement : [
+        {
+          Action : [
+            "dynamodb:DescribeTable",
+            "dynamodb:PutItem",
+            "dynamodb:GetItem",
+            "dynamodb:UpdateItem",
+            "dynamodb:DeleteItem",
+            "dynamodb:Query",
+            "dynamodb:Scan",
+            "dynamodb:BatchGetItem",
+            "dynamodb:BatchWriteItem",
+            "dynamodb:ConditionCheckItem"
+          ],
+          Effect : "Allow",
+          Resource : [
+            "${aws_dynamodb_table.orders.arn}"
+          ]
+        }
+      ]
+  })
+}
+
+
+#Dax Cluster and Related
+resource "aws_dax_subnet_group" "orders_dax" {
+  name       = "orders-dax"
+  subnet_ids = var.private_subnets
+}
+
+
+resource "aws_dax_cluster" "orders_dax_cluster" {
+  cluster_name       = "order-dax-cluster"
+  iam_role_arn       = aws_iam_role.dax_table_role.arn
+  node_type          = "dax.t3.medium"
+  replication_factor = 4
+  security_group_ids = [aws_security_group.default_security_group.id]
+  subnet_group_name = aws_dax_subnet_group.orders_dax.name
+}
+
+
+
--- infrastructure/outputs.tf
+++ infrastructure/outputs.tf
@@ -7,3 +7,32 @@ output "memorydb_endpoint" {
   description = "The endpoint of the cluster"
   value       = aws_memorydb_cluster.reinvent_memdb.cluster_endpoint
 }
+
+output "dax_endpoint" {
+  description = "The DAX cluster endpoint"
+  value       = aws_dax_cluster.orders_dax_cluster.configuration_endpoint
+}
+
+
+output "dax_address" {
+  description = "The DAX cluster endpoint URL"
+  value       = aws_dax_cluster.orders_dax_cluster.cluster_address
+}
+
+output "dax_port" {
+  description = "The DAX cluster endpoint URL"
+  value       = aws_dax_cluster.orders_dax_cluster.port
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
