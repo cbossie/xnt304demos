fabd4f360e1ce01ff2c263d95d1622e9714ba0ea
workshop-1c71e191-b6a2-4a53-8cf0-4fea15eb651b <user@codecommit.aws>
2022-11-28 10:52:01 -0800

    Update

--- dynamodb/GlobalTables/Program.cs
+++ dynamodb/GlobalTables/Program.cs
@@ -3,10 +3,13 @@ using Amazon.DynamoDBv2.DataModel;
 using GlobalTables.Model;
 
 
-int msSteps = 1;
+int msSteps = 10;
 int elapsed = 0;
 string oldSerial = Guid.NewGuid().ToString();
 string newSerial = Guid.NewGuid().ToString();
+string newSerial2 = Guid.NewGuid().ToString();
+
+string id = $"Batman - Clone number {DateTime.Now.Millisecond}";
 
 // Create Client from each
 IAmazonDynamoDB useast1Client = new AmazonDynamoDBClient(Amazon.RegionEndpoint.USEast1);
@@ -17,7 +20,7 @@ DynamoDBContext euwest1Context = new DynamoDBContext(euwest1Client);
 // Create an item
 Character chr = new Character
 {
-    Name = "Global Avenger",
+    Name = id,
     Role = "Superhero",
     Origin = "Krypton",
     Items = new List<string> { "Wand", "Battleax" },
@@ -31,9 +34,8 @@ await euwest1Context.SaveAsync(chr);
 chr.SerialNumber = newSerial;
 
 Console.WriteLine($"{Environment.NewLine}{Environment.NewLine}");
-Console.WriteLine($"Will update the serial number, then query the global table every 10 milliseconds until the serial number matches the new value {newSerial}");
-
-Console.WriteLine("Beginning Process");
+Console.WriteLine();
+Console.WriteLine("Beginning CREATE Process");
 Console.WriteLine($"Old Serial Number = {oldSerial}");
 Character charEurope;
 await euwest1Context.SaveAsync(chr);
@@ -44,8 +46,26 @@ do
 
     elapsed += msSteps;
 
-} while (charEurope.SerialNumber != newSerial);
+} while (charEurope == null || charEurope.SerialNumber != newSerial);
 
-Console.WriteLine($"After {elapsed} ms, the new serial number matches.");
+Console.WriteLine($"After {elapsed} ms, the item was created in the other region.");
 
+//UPDATE PROCESS
 
+Console.WriteLine("----------------------------------------------------");
+Console.WriteLine();
+Console.WriteLine("Beginning UPDATE Process");
+Console.WriteLine($"Old Serial Number = {chr.SerialNumber}");
+chr.SerialNumber = newSerial2;
+elapsed = 0;
+await euwest1Context.SaveAsync(chr);
+do
+{
+    await Task.Delay(msSteps);
+    charEurope = await useast1Context.LoadAsync<Character>(chr.Name);
+
+    elapsed += msSteps;
+
+} while (charEurope == null || charEurope.SerialNumber != newSerial2);
+Console.WriteLine($"After {elapsed} ms, the item was updated in the other region");
+Console.WriteLine("----------------------------------------------------");
